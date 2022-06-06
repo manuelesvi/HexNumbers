38e33ba7ba85a37bfe4da437884b1135dc937356
Manuel Espinosa <manuel.espinosa@live.com>
2022-06-06 12:30:49 -0500

    Add project files.

---
+++ HexNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+var sb = new StringBuilder();
+for (byte i = 0; i < byte.MaxValue; i++)
+{
+    if (i > 0)
+        sb.Append(' ');
+
+    if (i % 16 == 0)
+        sb.AppendLine();
+    
+    sb.AppendFormat("{0:X2}", i);
+}
+sb.AppendFormat(" {0:X2}", byte.MaxValue);
+
+Console.WriteLine(sb.ToString());
+Console.ReadKey();
