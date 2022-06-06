e4182cf3dbc4df4c4dd173e645230299520110e6
Manuel Espinosa <manuel.espinosa@live.com>
2022-06-06 15:31:24 -0500

    Add decimal count at end of row.

--- HexNumbers/Program.cs
+++ HexNumbers/Program.cs
@@ -6,12 +6,16 @@ for (byte i = 0; i < byte.MaxValue; i++)
     if (i > 0)
         sb.Append(' ');
 
-    if (i % 16 == 0)
+    if (i > 0 && i % 16 == 0)
+    {
+        sb.AppendFormat(" ({0})", i);
         sb.AppendLine();
+    }
     
     sb.AppendFormat("{0:X2}", i);
 }
 sb.AppendFormat(" {0:X2}", byte.MaxValue);
+sb.AppendFormat("  ({0})", byte.MaxValue + 1);
 
 Console.WriteLine(sb.ToString());
 Console.ReadKey();
